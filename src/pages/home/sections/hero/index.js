95d704942e3e4d15e9bb3e192a9516ee6e99b86c
bruce grover lee <brucegrover.lee@gmail.com>
2019-07-13 17:53:54 -0500

    looks great the hero

--- src/pages/home/sections/hero/index.js
+++ src/pages/home/sections/hero/index.js
@@ -16,7 +16,13 @@ import Hero from './hero.jsx';
 /** styles */
 import './hero.scss';
 /** files */
-import editorPicture from './images/editor-transparent.png';
+import airportNightPicture from './images/airport-night.jpg';
+import ideaComPicture from './images/idea.com.svg';
+import yourPicture from './images/YOUR.svg';
+import ideaPicture from './images/IDEA.svg';
+import takeOffPicture from './images/TAKE_OFF.svg';
+import herePicture from './images/HERE.svg';
+import buttonPicture from './images/button.svg';
 /** strings */
 import strings from './hero.strings.json';
 
@@ -35,9 +41,8 @@ class HeroContainer extends React.Component {
         <Controller>
           <Scene
             triggerHook="onLeave"
-            duration={2500}
+            duration={5000}
             pin={'#hero-section'}
-            pinSettings={true}
           >
             {(progress) => (
               <Hero>
@@ -46,22 +51,57 @@ class HeroContainer extends React.Component {
                   paused
                   target={
                     <div className="animation__editor">
-                      <img src={editorPicture}/>
-                      <Timeline totalProgress={progress} paused>
-                        <Tween
-                            from={{ left: '10%', top: '60%' }}
-                            to={{ left: '60%', top: '10%' }}
-                        >
-                          <div className="animation">
-                            holiiiiiiii              
-                          </div>
-                        </Tween>
-                      </Timeline>
+                      <div className="animation__editor__wrapper">
+                        <Timeline totalProgress={progress} paused>
+                          <Tween
+                              from={{ opacity: 0, right: '-110%' }}
+                              to={{ opacity: 1, right: '0%' }}
+                          >
+                            <img src={airportNightPicture} style={{ width: '100%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '-60%' }}
+                              to={{ opacity: 1, top: '51%' }}
+                          >
+                            <img src={herePicture} style={{ width: '12.5%', marginLeft: '11%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '-60%' }}
+                              to={{ opacity: 1, top: '44%' }}
+                          >
+                            <img src={takeOffPicture} style={{ width: '23.5%', marginLeft: '11%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '-60%' }}
+                              to={{ opacity: 1, top: '36.5%' }}
+                          >
+                            <img src={ideaPicture} style={{ width: '12%', marginLeft: '11%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '-60%' }}
+                              to={{ opacity: 1, top: '29.6348%' }}
+                          >
+                            <img src={yourPicture} style={{ width: '15%', marginLeft: '10.5%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '-60%' }}
+                              to={{ opacity: 1, top: '6.6348%' }}
+                          >
+                            <img src={ideaComPicture} style={{ width: '15%', marginLeft: '10.5%' }}/>
+                          </Tween>
+                          <Tween
+                              from={{ opacity: 0, top: '160%' }}
+                              to={{ opacity: 1, top: '62.6348%' }}
+                          >
+                            <img src={buttonPicture} style={{ width: '29.5%', marginLeft: '10.5%' }}/>
+                          </Tween>
+                        </Timeline>
+                      </div>
                     </div>
                   }
                 >
                   <Tween
-                    duration={5}
+                    duration={7}
                   />
                   <Tween
                     to={{css: { transform: 'rotateY(0deg) rotateX(0deg)' }}}
