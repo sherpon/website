5054e19fd48f20d1617a0cbe732bb4b7160de591
bruce grover lee <brucegrover.lee@gmail.com>
2019-07-19 07:35:13 -0500

    fix: cta

--- src/pages/home/sections/cta/index.js
+++ src/pages/home/sections/cta/index.js
@@ -16,6 +16,9 @@ import Cta from './cta.jsx';
 /** styles */
 import './cta.scss';
 /** files */
+import line1Picture from './images/line1.webp';
+import line2Picture from './images/line2.webp';
+import line3Picture from './images/line3.webp';
 /** strings */
 import strings from './cta.strings.json';
 
@@ -35,16 +38,23 @@ const CtaContainer = ({language}) => {
         {(progress) => (
           <div className="cta-container-section__wrapper">
             <Cta strings={strings[language]} >
+            <div className="cta-container-section__wrapper__perspective">
             <Timeline totalProgress={progress} paused>
               <Tween
-                  from={{ x: '10%', top: '60%' }}
-                  to={{ x: '60%', top: '10%' }}
+                  to={{ opacity: 1, left: '-20%' }}
               >
-                <div className="cta-container-section__wrapper__animation">
-                  holi            
+                <div className="cta-container-section__wrapper__animation" style={{ opacity: 0, top: '0px', left: '-80%' }}>
+                  <img src={line1Picture} alt="sherpon sign up" style={{ width: '669.15px' }}/>          
+                </div>
+                <div className="cta-container-section__wrapper__animation" style={{ opacity: 0, top: '111px', left: '-100%' }}>
+                  <img src={line2Picture} alt="sherpon sign up" style={{ width: '839.8px', left: '-10%' }}/>            
+                </div>
+                <div className="cta-container-section__wrapper__animation" style={{ opacity: 0, top: '222px', left: '-90%' }}>
+                  <img src={line3Picture} alt="sherpon sign up" style={{ width: '669.15px' }}/>           
                 </div>
               </Tween>
             </Timeline>
+            </div>
             </Cta>
           </div>
         )}
